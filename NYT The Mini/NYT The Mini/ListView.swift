ebf3f45edadf7b49ad20241a09008d6244c1666c
Sanjana Gopalswamy <sgopal@unc.edu>
2024-04-22 01:11:42 -0400

    finished ListView

---
+++ NYT The Mini/NYT The Mini/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  NYT The Mini
+//
+//  Created by Sanjana G on 4/22/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ NYT The Mini/NYT The Mini/NavBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavBar.swift
+//  NYT The Mini
+//
+//  Created by Sanjana G on 4/22/24.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavBar()
+}
